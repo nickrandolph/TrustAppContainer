a3d12251db9523c5baaf6b4461a25fd6833f1fd2
Nick Randolph <nick@builttoroam.com>
2021-01-11 10:39:26 +1100

    Add project files.

---
+++ FullTrustUWPApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FullTrustUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TempWriteButtonClick(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(@"c:\temp\dummyoutput.txt", "File contents");
+        }
+
+        private void AppDataWriteButtonClick(object sender, RoutedEventArgs e)
+        {
+            var file = System.IO.Path.Combine(ApplicationData.Current.LocalFolder.Path, $"dummyoutput.txt");
+
+            File.WriteAllText(file, "File contents");
+
+        }
+    }
+}
---
+++ PartialTrustWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Storage;
+
+namespace PartialTrustWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TempWriteButtonClick(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(@"c:\temp\dummyoutput.txt", "File contents");
+        }
+
+        private void AppDataWriteButtonClick(object sender, RoutedEventArgs e)
+        {
+            var file = System.IO.Path.Combine(ApplicationData.Current.LocalFolder.Path, $"dummyoutput.txt");
+
+            File.WriteAllText(file, "File contents");
+
+        }
+    }
+}
